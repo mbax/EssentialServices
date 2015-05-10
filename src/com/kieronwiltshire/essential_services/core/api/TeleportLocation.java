3511d9daf5606bbcdfe4d50d96d233ba42ec912b
Kieron Wiltshire <kieron.wiltshire@outlook.com>
2015-05-10 14:56:37 +0100

    Renamed Warp

--- src/com/kieronwiltshire/essential_services/core/api/TeleportLocation.java
+++ src/com/kieronwiltshire/essential_services/core/api/TeleportLocation.java
@@ -4,7 +4,7 @@ import com.google.common.base.Optional;
 import com.kieronwiltshire.essential_services.core.api.SpawnLocation;
 import org.spongepowered.api.entity.player.User;
 
-public interface Warp {
+public interface TeleportLocation {
 
     /**
      * Get the name
@@ -19,13 +19,6 @@ public interface Warp {
      * @return The person who created the warp
      */
     User creator();
-
-    /**
-     * Get the modifier
-     *
-     * @return The person who last modified the warp
-     */
-    Optional<User> modifier();
 
     /**
      * Get the spawn
